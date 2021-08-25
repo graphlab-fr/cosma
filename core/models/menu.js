237bf7efa992c128d7a09fdf53e83fc5a37806b6
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2021-08-25 16:06:12 +0200

    [edit] menu for MacOs
    [edit] menu for MacOs

--- core/models/menu.js
+++ core/models/menu.js
@@ -18,10 +18,120 @@ const Config = require('./config')
 module.exports = function () {
     const config = new Config().opts;
 
-    let template = [
-        {
-            label: 'Cosma',
+    let template = [];
+
+    if (process.platform === 'darwin') { // on MacOS
+        template.push({
+            label: app.name,
             submenu: [
+                {
+                    label: 'À propos',
+                    role: 'about'
+                },
+                {
+                    label: 'Préférences…',
+                    accelerator: 'CommandOrControl+O',
+                    role: 'options',
+                    click () {
+                        require('../views/config/index')();
+                    }
+                },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                {
+                    label: 'Masquer Cosma',
+                    role: 'hide'
+                },
+                { 
+                    label: 'Masquer les autres',
+                    role: 'hideothers'
+                },
+                {
+                    label: 'Tout afficher',
+                    role: 'unhide' 
+                },
+                { type: 'separator' },
+                {
+                    label: 'Quitter Cosma',
+                    role: 'quit'
+                }
+            ]
+        },
+        {
+            label: 'Fichier',
+            submenu: [
+                {
+                    label: 'Nouveau cosmoscope…',
+                    accelerator: 'CommandOrControl+N',
+                    role: 'new-cosmoscope',
+                    click () {
+                        require('../views/cosmoscope')();
+                    }
+                },
+                {
+                    label: 'Nouvelle fiche…',
+                    role: 'new-record',
+                    click () {
+                        require('../views/record/index')();
+                    }
+                },
+                {
+                    label: 'Exporter cosmoscope…',
+                    accelerator: 'CommandOrControl+E',
+                    role: 'export-cosmoscope',
+                    click (item, window) {
+                        require('../views/export/index')(window);
+                    }
+                },
+                { type: 'separator' },
+                {
+                    label: 'Historique…',
+                    accelerator: 'CommandOrControl+H',
+                    role: 'history',
+                    click () {
+                        require('../views/history/index')();
+                    }
+                }
+            ]
+        });
+    } else {
+        template.push({
+            label: 'Fichier',
+            submenu: [
+                {
+                    label: 'Nouveau cosmoscope…',
+                    accelerator: 'CommandOrControl+N',
+                    role: 'new-cosmoscope',
+                    click () {
+                        require('../views/cosmoscope')();
+                    }
+                },
+                {
+                    label: 'Nouvelle fiche…',
+                    role: 'new-record',
+                    click () {
+                        require('../views/record/index')();
+                    }
+                },
+                {
+                    label: 'Exporter cosmoscope…',
+                    accelerator: 'CommandOrControl+E',
+                    role: 'export-cosmoscope',
+                    click (item, window) {
+                        require('../views/export/index')(window);
+                    }
+                },
+                { type: 'separator' },
+                {
+                    label: 'Historique…',
+                    accelerator: 'CommandOrControl+H',
+                    role: 'history',
+                    click () {
+                        require('../views/history/index')();
+                    }
+                },
+                { type: 'separator' },
                 {
                     label: 'Préférences…',
                     accelerator: 'CommandOrControl+O',
@@ -37,106 +147,71 @@ module.exports = function () {
                     click () {
                         app.quit();
                     }
-                }
+                },
             ]
-        },
-        {
-            label: 'Fichier',
-            submenu: [
-                {
-                    label: 'Nouvelle fiche…',
-                    role: 'new-record',
-                    click () {
-                        require('../views/record/index')();
-                    }
-                },
-                {
-                    label: 'Nouveau cosmoscope…',
-                    accelerator: 'CommandOrControl+N',
-                    role: 'new-cosmoscope',
-                    click () {
-                        require('../views/cosmoscope')();
-                    }
-                },
-                {
-                    label: 'Exporter cosmoscope…',
-                    accelerator: 'CommandOrControl+E',
-                    role: 'export-cosmoscope',
-                    click (item, window) {
-                        require('../views/export/index')(window);
-                    }
-                },
-                {
-                    label: 'Historique…',
-                    accelerator: 'CommandOrControl+H',
-                    role: 'history',
-                    click () {
-                        require('../views/history/index')();
-                    }
+        });
+    }
+
+    template.push({
+        label: 'Affichage',
+        submenu: [
+            {
+                label: 'En arrière',
+                accelerator: 'CommandOrControl+Left',
+                role: 'back',
+                click(item, window) {
+                    if (mainWindow.webContents.canGoBack()) { mainWindow.webContents.goBack() };
                 }
-            ]
-        },
-        {
-            label: 'Affichage',
-            submenu: [
-                {
-                    label: 'En arrière',
-                    accelerator: 'CommandOrControl+Left',
-                    role: 'back',
-                    click(item, window) {
-                        if (mainWindow.webContents.canGoBack()) { mainWindow.webContents.goBack() };
-                    }
-                },
-                {
-                    label: 'En avant',
-                    accelerator: 'CommandOrControl+Right',
-                    role: 'forward',
-                    click(item, window) {
-                        if (mainWindow.webContents.canGoForward()) { window.webContents.goForward() };
-                    }
-                },
-                {
-                    label: 'Imprimer',
-                    accelerator: 'CommandOrControl+P',
-                    role: 'print',
-                    click(item, window) {
-                        mainWindow.webContents.printToPDF({
-                            headerFooter: {
-                                title: mainWindow.title,
-                                url: 'https://cosma.netlify.app/'
-                            },
-                            pageSize: 'A4'
-                        })
-                        .then(pdfData => {
-                            dialog.showSaveDialog(mainWindow, {
-                                title: 'Enregistrer le PDF',
-                                defaultPath: path.join(app.getPath('documents'), `${mainWindow.title}.pdf`),
-                                properties: ['createDirectory', 'showOverwriteConfirmation']
-                            }).then((response) => {
-                                if (response.canceled === false) {
-                                    fs.writeFile(response.filePath, pdfData, (err) => {
-                                        if (err) { throw `Erreur d'enregistrement de l'impression PDF : ${err}`; }
-                                    })
-                                }
-                            });
-                        })
-                        .catch(err => {
-                            throw `Erreur d'impression du PDF : ${err}`;
-                        })
-                    }
+            },
+            {
+                label: 'En avant',
+                accelerator: 'CommandOrControl+Right',
+                role: 'forward',
+                click(item, window) {
+                    if (mainWindow.webContents.canGoForward()) { window.webContents.goForward() };
                 }
-            ]
-        },
-        {
-            label: 'Aide',
-            submenu: [
-                {
-                    label: 'Aide Cosma',
-                    role: 'doc'
+            },
+            {
+                label: 'Imprimer',
+                accelerator: 'CommandOrControl+P',
+                role: 'print',
+                click(item, window) {
+                    mainWindow.webContents.printToPDF({
+                        headerFooter: {
+                            title: mainWindow.title,
+                            url: 'https://cosma.netlify.app/'
+                        },
+                        pageSize: 'A4'
+                    })
+                    .then(pdfData => {
+                        dialog.showSaveDialog(mainWindow, {
+                            title: 'Enregistrer le PDF',
+                            defaultPath: path.join(app.getPath('documents'), `${mainWindow.title}.pdf`),
+                            properties: ['createDirectory', 'showOverwriteConfirmation']
+                        }).then((response) => {
+                            if (response.canceled === false) {
+                                fs.writeFile(response.filePath, pdfData, (err) => {
+                                    if (err) { throw `Erreur d'enregistrement de l'impression PDF : ${err}`; }
+                                })
+                            }
+                        });
+                    })
+                    .catch(err => {
+                        throw `Erreur d'impression du PDF : ${err}`;
+                    })
                 }
-            ]
-        }
-    ];
+            }
+        ]
+    },
+    {
+        label: 'Aide',
+        submenu: [
+            {
+                label: 'Aide Cosma',
+                role: 'doc'
+            }
+        ]
+    });
 
     if (config.devtools === true) {
         template.push({
