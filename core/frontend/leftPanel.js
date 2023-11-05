ba19ba0ed2b028f315da3a0abecedb0f44d6c1b6
Myllaume <guillaume.brioudes@myllaume.fr>
2023-11-05 14:40:11 +0100

    feat: move graph controls

--- core/frontend/leftPanel.js
+++ core/frontend/leftPanel.js
@@ -1,9 +1,11 @@
-const closeLeftSideButton = document.getElementById('close-left-side');
-const menuContainer = document.getElementById('menu-container');
-
 window.addEventListener('DOMContentLoaded', () => {
+  const closeLeftSideButton = document.getElementById('close-left-side');
+  const menuContainer = document.getElementById('menu-container');
+  const graphContolsContainer = document.getElementById('graph-controls');
+
   closeLeftSideButton.addEventListener('click', () => {
     closeLeftSideButton.classList.toggle('active');
     menuContainer.classList.toggle('active');
+    graphContolsContainer.classList.toggle('move');
   });
 });
