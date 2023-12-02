42719e23eef49f340c711c4f2a2f2740c9d6a737
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2023-12-02 12:04:37 +0100

    feat(front): zoom to node

--- core/frontend/zoom.js
+++ core/frontend/zoom.js
@@ -1,8 +1,9 @@
 import * as d3 from 'd3';
 
 import View from './view';
-import { svg, translate } from './graph';
+import { svg, translate, nodes } from './graph';
 import hotkeys from 'hotkeys-js';
+import { getRecordIdFromHash } from './records';
 
 const zoomMax = 10,
   zoomMin = 1,
@@ -13,6 +14,7 @@ const zoom = d3
   .scaleExtent([zoomMin, zoomMax])
   .on('zoom', () => {
     const { x, y, k } = d3.event.transform;
+    console.log(k);
     View.position.x = x || 0;
     View.position.y = y || 0;
     View.position.zoom = k || 1;
@@ -39,6 +41,33 @@ function zoomReset() {
   );
   translate();
 }
+
+/**
+ * Zoom to a node from its coordinates
+ * @param {string} nodeId
+ */
+
+function zoomToNode(nodeId) {
+  const { x, y } = nodes.find(({ id }) => id === nodeId);
+
+  const meanX = d3.mean(nodes, (d) => d.x);
+  const meanY = d3.mean(nodes, (d) => d.y);
+
+  const zoomScale = 2;
+
+  svg.call(
+    zoom.transform,
+    d3.zoomIdentity.translate(-(x * zoomScale - meanX), -(y * zoomScale - meanY)).scale(zoomScale),
+  );
+  translate();
+}
+
+hotkeys('c', (e) => {
+  e.preventDefault();
+  const recordId = getRecordIdFromHash();
+
+  zoomToNode(recordId);
+});
 
 hotkeys('e,alt+r', (e) => {
   e.preventDefault();
