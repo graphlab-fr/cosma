99e420cb7322fa881bd595e6035553ad7efc4fe9
Myllaume <guillaume.brioudes@myllaume.fr>
2024-05-01 12:04:07 +0200

    fix: make user data dir

--- controllers/user-data-dir.js
+++ controllers/user-data-dir.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
-const Config = require('../models/config-cli');
+const Config = require('../core/models/config');
 
 module.exports = function () {
-  if (fs.existsSync(Config.pathConfigDir)) {
+  if (fs.existsSync(Config.configDirPath)) {
     console.log(
       'User data directory already exists at',
-      ['\x1b[2m', Config.pathConfigDir, '\x1b[0m'].join(''),
+      ['\x1b[2m', Config.configDirPath, '\x1b[0m'].join(''),
     );
   } else {
-    fs.mkdir(Config.pathConfigDir, { recursive: true }, (err) => {
+    fs.mkdir(Config.configDirPath, { recursive: true }, (err) => {
       if (err) {
         console.error(
           ['\x1b[31m', 'Err.', '\x1b[0m'].join(''),
@@ -18,7 +18,7 @@ module.exports = function () {
       }
       console.log(
         ['\x1b[32m', 'User data directory created at', '\x1b[0m'].join(''),
-        ['\x1b[2m', Config.pathConfigDir, '\x1b[0m'].join(''),
+        ['\x1b[2m', Config.configDirPath, '\x1b[0m'].join(''),
       );
     });
   }
