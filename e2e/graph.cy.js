bdfc9c5466a677b08e45e305e0f15310b0045aaa
Myllaume <guillaume.brioudes@myllaume.fr>
2024-05-22 12:47:33 +0200

    test: graph

---
+++ e2e/graph.cy.js
@@ -0,0 +1,27 @@
+describe('graph', () => {
+  beforeEach(() => {
+    cy.visit('e2e/citeproc/cosmoscope.html');
+  });
+
+  it('should get one node per record', () => {
+    const numberOfRecord = 6;
+    cy.get('[data-node]').should('have.length', numberOfRecord);
+  });
+
+  it('should open record on click on node', () => {
+    cy.get('[data-node="evergreen notes"]').as('node');
+    cy.get('@node').find('text').should('have.text', 'Evergreen notes').click();
+    cy.get('.record.active .record-title').should('have.text', 'Evergreen notes');
+  });
+
+  it('should hilight links from and to clicked node', () => {
+    cy.get('[data-node="evergreen notes"]').as('node');
+    cy.get('@node').click();
+    cy.get('[data-source="evergreen notes"]')
+      .should('have.length', 1)
+      .each((elt) => expect(elt.attr('style')).to.contain('highlight'));
+    cy.get('[data-target="evergreen notes"]')
+      .should('have.length', 3)
+      .each((elt) => expect(elt.attr('style')).to.contain('highlight'));
+  });
+});
