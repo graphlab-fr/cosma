3136831cf9faef3ed5e4e93cd3deda9d084adb5f
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2024-08-10 12:50:34 +0200

    test: index

--- e2e/leftPanel.cy.js
+++ e2e/leftPanel.cy.js
@@ -11,4 +11,43 @@ describe('left panel', () => {
     cy.get('#close-left-side').click();
     cy.get('@leftPanel').should('have.class', 'active');
   });
+
+  describe('records index', () => {
+    /** @type {string[]} */
+    function shouldIndexHasRecords(labels) {
+      cy.get('[data-index]:visible span:nth-child(2)')
+        .should('have.length', labels.length)
+        .each((elt, i) => {
+          expect(elt.text()).to.equal(labels[i]);
+        });
+    }
+
+    beforeEach(() => {
+      cy.contains('Index').click();
+    });
+
+    it('should display each record with alphabetical order', () => {
+      shouldIndexHasRecords([
+        'Augmenting Human Intellect: A Conceptual Framework',
+        'Evergreen note titles are like APIs',
+        'Evergreen notes',
+        'Evergreen notes should be atomic',
+        'Evergreen notes should be concept-oriented',
+        'Evergreen notes should be densely linked',
+        'How can we develop transformative tools for thought?',
+        'Tools for thought',
+      ]);
+    });
+
+    it('should display each unfliterd record', () => {
+      cy.get('#types-form').contains('insight').click();
+
+      shouldIndexHasRecords([
+        'Augmenting Human Intellect: A Conceptual Framework',
+        'Evergreen notes',
+        'How can we develop transformative tools for thought?',
+        'Tools for thought',
+      ]);
+    });
+  });
 });
