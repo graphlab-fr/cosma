d37693924ddf8afec29735a3ca80839269224346
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2025-08-08 13:56:35 +0200

    refa: filters test

--- e2e/filters.cy.js
+++ e2e/filters.cy.js
@@ -3,11 +3,6 @@ const data = require('./batch/data.json');
 const allTitles = data.map(({ title }) => title);
 
 describe('Filters', () => {
-  beforeEach(() => {
-    cy.visit('temp/batch.html');
-    cy.get('#types-form').as('filtersContainer');
-  });
-
   /** @param {string[]} names */
   function assertFiltersAreChecked(names) {
     cy.get('@filtersContainer')
@@ -23,256 +18,265 @@ describe('Filters', () => {
     cy.get('@filtersContainer').contains(name).click();
   }
 
-  const filters = ['œuvre', 'personne', 'institution', 'otlet'];
-
-  it('should display filter for each type', () => {
-    cy.get('@filtersContainer').find('.filter input').should('have.length', filters.length);
-
-    filters.forEach((filterName) => {
-      cy.get('@filtersContainer').contains(filterName).should('be.visible');
-    });
-  });
-
-  it('should check all filters if no URL params', () => {
-    assertFiltersAreChecked(filters);
-  });
-
-  it('should apply filters as URL params on click on view action', () => {
-    clickOnFilter('œuvre');
-    clickOnFilter('personne');
-    clickOnFilter('institution');
-
-    cy.contains("Appliquer la vue actuelle à l'URL").click();
-
-    cy.location('search').should((loc) => {
-      expect(loc).to.equal('?filters=otlet');
-    });
-  });
-
-  it('should check only filters from URL params', () => {
-    cy.visit('temp/batch.html?filters=personne-otlet');
-    assertFiltersAreChecked(['personne', 'otlet']);
-  });
-
-  it('should uncheck input on filter label click', () => {
-    cy.get('@filtersContainer').find('.filter').first().as('filter');
-    cy.get('@filter').find('input').should('be.checked');
-    cy.get('@filter').click();
-    cy.get('@filter').find('input').should('not.be.checked');
-  });
-
-  it('should toggle filter toggle node visibility', () => {
-    cy.shouldGraphHasNodes(allTitles);
-
-    clickOnFilter('œuvre');
-    assertFiltersAreChecked(['personne', 'institution', 'otlet']);
-
-    cy.shouldGraphHasNodes(allTitles.filter((title) => title !== 'CDU'));
-
-    clickOnFilter('œuvre');
-    assertFiltersAreChecked(['personne', 'œuvre', 'institution', 'otlet']);
-
-    cy.shouldGraphHasNodes(allTitles);
-
-    clickOnFilter('personne');
-    assertFiltersAreChecked(['œuvre', 'institution', 'otlet']);
-
-    cy.shouldGraphHasNodes(['Mundaneum', 'CDU', 'Paul Otlet']);
-
-    clickOnFilter('otlet');
-    assertFiltersAreChecked(['œuvre', 'institution']);
-
-    cy.shouldGraphHasNodes(['Mundaneum', 'CDU']);
-  });
-
-  it('should filter node with several types', () => {
-    cy.shouldGraphHasNodes(allTitles);
-
-    clickOnFilter('otlet');
-    cy.shouldGraphHasNodes(allTitles);
-
-    clickOnFilter('personne');
-    cy.shouldGraphHasNodes(['Mundaneum', 'CDU']);
-
-    clickOnFilter('otlet');
-    cy.shouldGraphHasNodes(['Paul Otlet', 'Mundaneum', 'CDU']);
-
-    clickOnFilter('otlet');
-    cy.shouldGraphHasNodes(['Mundaneum', 'CDU']);
-
-    clickOnFilter('personne');
-    cy.shouldGraphHasNodes(allTitles);
-  });
-
-  describe('with alt key', () => {
-    it('should uncheck all inputs but not clicked one if alt key is pressed', () => {
-      assertFiltersAreChecked(filters);
-
-      cy.get('@filtersContainer').contains('personne').click({ altKey: true });
-
-      assertFiltersAreChecked(['personne']);
+  describe('on batch file', () => {
+    beforeEach(() => {
+      cy.visit('temp/batch.html');
+      cy.get('#types-form').as('filtersContainer');
     });
 
-    it('should check all inputs if alt key is pressed on a second click on same filter', () => {
-      assertFiltersAreChecked(filters);
+    const filters = ['œuvre', 'personne', 'institution', 'otlet'];
 
-      cy.get('@filtersContainer').contains('personne').click({ altKey: true });
+    it('should display filter for each type', () => {
+      cy.get('@filtersContainer').find('.filter input').should('have.length', filters.length);
 
-      assertFiltersAreChecked(['personne']);
+      filters.forEach((filterName) => {
+        cy.get('@filtersContainer').contains(filterName).should('be.visible');
+      });
+    });
 
-      cy.get('@filtersContainer').contains('personne').click({ altKey: true });
-
+    it('should check all filters if no URL params', () => {
       assertFiltersAreChecked(filters);
     });
-  });
-});
 
-describe('on Citeproc file', () => {
-  beforeEach(() => {
-    cy.visit('temp/citeproc.html');
-    cy.get('#types-form').as('filtersContainer');
-    cy.get('#link-types-form').as('linkFiltersContainer');
-  });
+    it('should apply filters as URL params on click on view action', () => {
+      clickOnFilter('œuvre');
+      clickOnFilter('personne');
+      clickOnFilter('institution');
 
-  const nodeFilters = [
-    {
-      label: 'reference',
-      count: 2,
-    },
-    {
-      label: 'insight',
-      count: 4,
-    },
-    {
-      label: 'concept',
-      count: 2,
-    },
-  ];
+      cy.contains("Appliquer la vue actuelle à l'URL").click();
 
-  const linkFilters = [
-    {
-      label: 'undefined',
-      count: 5,
-    },
-    {
-      label: 'adjacent-concept',
-      count: 1,
-    },
-    {
-      label: 'a',
-      count: 1,
-    },
-  ];
+      cy.location('search').should((loc) => {
+        expect(loc).to.equal('?filters=otlet');
+      });
+    });
 
-  it('should display number of node types', () => {
-    cy.get('.node-types-title').find('.badge').should('contain.text', nodeFilters.length);
-  });
+    it('should check only filters from URL params', () => {
+      cy.visit('temp/batch.html?filters=personne-otlet');
+      assertFiltersAreChecked(['personne', 'otlet']);
+    });
 
-  it('should display number of link types', () => {
-    cy.get('.link-types-title').find('.badge').should('contain.text', linkFilters.length);
-  });
+    it('should uncheck input on filter label click', () => {
+      cy.get('@filtersContainer').find('.filter').first().as('filter');
+      cy.get('@filter').find('input').should('be.checked');
+      cy.get('@filter').click();
+      cy.get('@filter').find('input').should('not.be.checked');
+    });
 
-  it('should display number of link for each filter', () => {
-    cy.get('@linkFiltersContainer').find('.badge').as('badges');
+    it('should toggle filter toggle node visibility', () => {
+      cy.shouldGraphHasNodes(allTitles);
 
-    cy.get('@badges').should('have.length', linkFilters.length);
-    linkFilters.forEach(({ label, count }, index) => {
-      cy.get('@badges').eq(index).should('contain', count);
+      clickOnFilter('œuvre');
+      assertFiltersAreChecked(['personne', 'institution', 'otlet']);
+
+      cy.shouldGraphHasNodes(allTitles.filter((title) => title !== 'CDU'));
+
+      clickOnFilter('œuvre');
+      assertFiltersAreChecked(['personne', 'œuvre', 'institution', 'otlet']);
+
+      cy.shouldGraphHasNodes(allTitles);
+
+      clickOnFilter('personne');
+      assertFiltersAreChecked(['œuvre', 'institution', 'otlet']);
+
+      cy.shouldGraphHasNodes(['Mundaneum', 'CDU', 'Paul Otlet']);
+
+      clickOnFilter('otlet');
+      assertFiltersAreChecked(['œuvre', 'institution']);
+
+      cy.shouldGraphHasNodes(['Mundaneum', 'CDU']);
+    });
+
+    it('should filter node with several types', () => {
+      cy.shouldGraphHasNodes(allTitles);
+
+      clickOnFilter('otlet');
+      cy.shouldGraphHasNodes(allTitles);
+
+      clickOnFilter('personne');
+      cy.shouldGraphHasNodes(['Mundaneum', 'CDU']);
+
+      clickOnFilter('otlet');
+      cy.shouldGraphHasNodes(['Paul Otlet', 'Mundaneum', 'CDU']);
+
+      clickOnFilter('otlet');
+      cy.shouldGraphHasNodes(['Mundaneum', 'CDU']);
+
+      clickOnFilter('personne');
+      cy.shouldGraphHasNodes(allTitles);
+    });
+
+    describe('with alt key', () => {
+      it('should uncheck all inputs but not clicked one if alt key is pressed', () => {
+        assertFiltersAreChecked(filters);
+
+        cy.get('@filtersContainer').contains('personne').click({ altKey: true });
+
+        assertFiltersAreChecked(['personne']);
+      });
+
+      it('should check all inputs if alt key is pressed on a second click on same filter', () => {
+        assertFiltersAreChecked(filters);
+
+        cy.get('@filtersContainer').contains('personne').click({ altKey: true });
+
+        assertFiltersAreChecked(['personne']);
+
+        cy.get('@filtersContainer').contains('personne').click({ altKey: true });
+
+        assertFiltersAreChecked(filters);
+      });
     });
   });
 
-  it('should display number of records for each filter', () => {
-    cy.get('@filtersContainer').find('.badge').as('badges');
-
-    cy.get('@badges').should('have.length', nodeFilters.length);
-    nodeFilters.forEach(({ label, count }, index) => {
-      cy.get('@badges').eq(index).should('contain', count);
+  describe('on citeproc file', () => {
+    beforeEach(() => {
+      cy.visit('temp/citeproc.html');
+      cy.get('#types-form').as('filtersContainer');
+      cy.get('#link-types-form').as('linkFiltersContainer');
     });
-  });
 
-  it('should display filter for each link type', () => {
-    cy.get('@linkFiltersContainer').find('.filter input').should('have.length', linkFilters.length);
+    const nodeFilters = [
+      {
+        label: 'concept',
+        count: 2,
+      },
+      {
+        label: 'insight',
+        count: 4,
+      },
+      {
+        label: 'reference',
+        count: 2,
+      },
+    ];
 
-    linkFilters.forEach(({ label }) => {
-      cy.get('@linkFiltersContainer').contains(label).should('be.visible');
+    const linkFilters = [
+      {
+        label: 'a',
+        count: 1,
+      },
+      {
+        label: 'adjacent-concept',
+        count: 1,
+      },
+      {
+        label: 'undefined',
+        count: 5,
+      },
+    ];
+
+    it('should display number of node types', () => {
+      cy.get('.node-types-title').find('.badge').should('contain.text', nodeFilters.length);
     });
-  });
 
-  it('should hide links on click on link type', () => {
-    cy.get('@linkFiltersContainer').find('.filter input').as('inputs');
+    it('should display number of link types', () => {
+      cy.get('.link-types-title').find('.badge').should('contain.text', linkFilters.length);
+    });
 
-    cy.get('[data-link]:visible').should('have.length', 7);
+    it('should display number of link for each filter', () => {
+      cy.get('@linkFiltersContainer').find('.badge').as('badges');
 
-    cy.get('@inputs').first().click();
-    cy.get('[data-link]:visible').should('have.length', 2);
-  });
+      cy.get('@badges').should('have.length', linkFilters.length);
+      linkFilters.forEach(({ count }, index) => {
+        cy.get('@badges').eq(index).should('contain', count);
+      });
+    });
 
-  describe('with alt key', () => {
-    function assertFiltersAreChecked(names) {
+    it('should display number of records for each filter', () => {
+      cy.get('@filtersContainer').find('.badge').as('badges');
+
+      cy.get('@badges').should('have.length', nodeFilters.length);
+      nodeFilters.forEach(({ count }, index) => {
+        cy.get('@badges').eq(index).should('contain', count);
+      });
+    });
+
+    it('should display filter for each link type', () => {
+      cy.get('@linkFiltersContainer')
+        .find('.filter input')
+        .should('have.length', linkFilters.length);
+
+      linkFilters.forEach(({ label }) => {
+        cy.get('@linkFiltersContainer').contains(label).should('be.visible');
+      });
+    });
+
+    it('should hide links on click on link type', () => {
+      cy.get('@linkFiltersContainer').find('.filter input').as('inputs');
+
+      cy.get('[data-link]:visible').should('have.length', 7);
+
+      cy.get('@inputs').last().click();
+      cy.get('[data-link]:visible').should('have.length', 2);
+    });
+
+    describe('with alt key for links', () => {
+      function assertLinkFiltersAreChecked(names) {
+        cy.get('@linkFiltersContainer')
+          .find('.filter input:checked')
+          .should('have.length', names.length)
+          .each((elt) => {
+            expect(elt.attr('name')).to.be.oneOf(names);
+          });
+      }
+
+      const linkNames = linkFilters.map(({ label }) => label);
+
+      it('should uncheck all link inputs but not clicked one if alt key is pressed', () => {
+        assertLinkFiltersAreChecked(linkNames);
+
+        cy.get('@linkFiltersContainer').contains('adjacent-concept').click({ altKey: true });
+
+        assertLinkFiltersAreChecked(['adjacent-concept']);
+      });
+
+      it('should check all link inputs if alt key is pressed on a second click on same filter', () => {
+        assertLinkFiltersAreChecked(linkNames);
+
+        cy.get('@linkFiltersContainer').contains('adjacent-concept').click({ altKey: true });
+
+        assertLinkFiltersAreChecked(['adjacent-concept']);
+
+        cy.get('@linkFiltersContainer').contains('adjacent-concept').click({ altKey: true });
+
+        assertLinkFiltersAreChecked(linkNames);
+      });
+    });
+
+    it('should reset all filters with alt+r shortcut', () => {
+      const allNodeFilters = nodeFilters.map(({ label }) => label);
+      const allLinkFilters = linkFilters.map(({ label }) => label);
+
+      cy.get('@filtersContainer')
+        .find('.filter input:checked')
+        .should('have.length', allNodeFilters.length);
+
       cy.get('@linkFiltersContainer')
         .find('.filter input:checked')
-        .should('have.length', names.length)
-        .each((elt) => {
-          expect(elt.attr('name')).to.be.oneOf(names);
-        });
-    }
+        .should('have.length', allLinkFilters.length);
 
-    const linkNames = linkFilters.map(({ label }) => label);
+      cy.get('@filtersContainer').contains('reference').click();
+      cy.get('@filtersContainer').contains('concept').click();
+      cy.get('@linkFiltersContainer').contains('undefined').click();
+      cy.get('@linkFiltersContainer').contains('a').click();
 
-    it('should uncheck all link inputs but not clicked one if alt key is pressed', () => {
-      assertFiltersAreChecked(linkNames);
+      cy.get('@filtersContainer')
+        .find('.filter input:checked')
+        .should('have.length', allNodeFilters.length - 2);
 
-      cy.get('@linkFiltersContainer').contains('adjacent-concept').click({ altKey: true });
+      cy.get('@linkFiltersContainer')
+        .find('.filter input:checked')
+        .should('have.length', allLinkFilters.length - 2);
 
-      assertFiltersAreChecked(['adjacent-concept']);
+      cy.get('body').type('{alt}r');
+
+      cy.get('@filtersContainer')
+        .find('.filter input:checked')
+        .should('have.length', allNodeFilters.length);
+
+      cy.get('@linkFiltersContainer')
+        .find('.filter input:checked')
+        .should('have.length', allLinkFilters.length);
     });
-
-    it('should check all link inputs if alt key is pressed on a second click on same filter', () => {
-      assertFiltersAreChecked(linkNames);
-
-      cy.get('@linkFiltersContainer').contains('adjacent-concept').click({ altKey: true });
-
-      assertFiltersAreChecked(['adjacent-concept']);
-
-      cy.get('@linkFiltersContainer').contains('adjacent-concept').click({ altKey: true });
-
-      assertFiltersAreChecked(linkNames);
-    });
-  });
-
-  it('should reset all filters with alt+r shortcut', () => {
-    const allNodeFilters = nodeFilters.map(({ label }) => label);
-    const allLinkFilters = linkFilters.map(({ label }) => label);
-
-    cy.get('@filtersContainer')
-      .find('.filter input:checked')
-      .should('have.length', allNodeFilters.length);
-
-    cy.get('@linkFiltersContainer')
-      .find('.filter input:checked')
-      .should('have.length', allLinkFilters.length);
-
-    cy.get('@filtersContainer').contains('reference').click();
-    cy.get('@filtersContainer').contains('concept').click();
-    cy.get('@linkFiltersContainer').contains('undefined').click();
-    cy.get('@linkFiltersContainer').contains('a').click();
-
-    cy.get('@filtersContainer')
-      .find('.filter input:checked')
-      .should('have.length', allNodeFilters.length - 2);
-
-    cy.get('@linkFiltersContainer')
-      .find('.filter input:checked')
-      .should('have.length', allLinkFilters.length - 2);
-
-    cy.get('body').type('{alt}r');
-
-    cy.get('@filtersContainer')
-      .find('.filter input:checked')
-      .should('have.length', allNodeFilters.length);
-
-    cy.get('@linkFiltersContainer')
-      .find('.filter input:checked')
-      .should('have.length', allLinkFilters.length);
   });
 });
