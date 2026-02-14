e268e2d4305036da88025925acb5f2d3053c661a
Myllaume <guillaume.brioudes@myllaume.fr>
2026-02-14 20:40:37 +0100

    chore: update deps

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('jest').Config} */
 const config = {
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(env-paths|is-safe-filename)/)',
+  ],
   moduleNameMapper: {
     '^d3$': '<rootDir>/node_modules/d3/dist/d3.js',
   },
