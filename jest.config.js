d431e65f89050d8aacd77941405f5371dce70dcd
Myllaume <guillaume.brioudes@myllaume.fr>
2026-03-15 14:30:31 +0100

    format

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,7 @@ const config = {
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(env-paths|is-safe-filename)/)',
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(env-paths|is-safe-filename)/)'],
   moduleNameMapper: {
     '^d3$': '<rootDir>/node_modules/d3/dist/d3.js',
   },
